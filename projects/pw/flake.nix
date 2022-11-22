b6dd79be8f17dcc3723184f055b8905b04263dc3
Markus Hauck <markus1189@gmail.com>
2022-11-22 08:19:07 +0100

    Flake for project

---
+++ projects/pw/flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config = {
+          allowUnfreePredicate = pkg:
+            builtins.elem (nixpkgs.lib.getName pkg) [ "vscode" "teams" ];
+        };
+      };
+      pyPkgs = pkgs.python39Packages;
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs-18_x
+          vscode
+          teams
+          python39
+          pyPkgs.pip
+          pyPkgs.autopep8
+          pipenv
+          glab
+        ];
+      };
+    };
+}
