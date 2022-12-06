659ac1bcbfec3d9dd64788415436caf43771cacb
Markus Hauck <markus1189@gmail.com>
2022-12-06 08:25:10 +0100

    zwift: init

---
+++ zwift/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+          myWine = pkgs.wineWowPackages.stagingFull;
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            myWine
+            winetricks
+            samba
+          ];
+
+          shellHook = ''
+            alias s="make start"
+            alias e="make stop"
+          '';
+        };
+      });
+}
